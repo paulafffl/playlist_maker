7b354a677f8b4a0c0f7ea14d08834845d072db97
Paula <paulafffl@gmail.com>
2020-03-01 11:37:23 +0000

    Passed Down Search Result and Rendered Result List

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import './TrackList.css'
 
-class TrackList extends React.Component{
-  render(){      
-    return(
+import './TrackList.css';
+
+import Track from '../Track/Track';
+ 
+class TrackList extends React.Component {
+  render() {      
+    console.log(this.props)
+    return (
       <div className="TrackList">
-         {/* <!-- You will add a map method that renders a set of Track components  --> */}
+         {
+           this.props.tracks.map(track => {
+             return <Track track={track}
+                           key={track.id} />
+           }) 
+         }
       </div>
     )
   }
